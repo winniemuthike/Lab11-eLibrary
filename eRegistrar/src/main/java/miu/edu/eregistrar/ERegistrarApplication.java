aeb7fabf97b3371fe9e14cb1fd9a147531c4d982
Winnie <wmuthike@miu.edu>
2022-01-28 00:36:26 -0600

    lab11 part 1, screenshots and eRegistrar

---
+++ eRegistrar/src/main/java/miu/edu/eregistrar/ERegistrarApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.eregistrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERegistrarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ERegistrarApplication.class, args);
+    }
+
+}
---
+++ eRegistrar/src/test/java/miu/edu/eregistrar/ERegistrarApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.eregistrar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERegistrarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
